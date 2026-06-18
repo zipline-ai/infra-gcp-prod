dca7385646b38a1490776cdc0af8b51f5669f6ca
chewy-zlai <chewys@zipline.ai>
2026-06-17 19:09:08 -0700

    Set eval to min of 1 instance

--- orchestration-gcp/cloud_run.tf
+++ orchestration-gcp/cloud_run.tf
@@ -1018,7 +1018,7 @@ resource "google_cloud_run_v2_service" "chronon_eval" {
     }
 
     scaling {
-      min_instance_count = 0
+      min_instance_count = 1
       max_instance_count = 3
     }
   }
